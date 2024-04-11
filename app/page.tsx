3ac23aa461b71816b3f01b47ed3eb16c9aae9b21
MatthewAndresWestMec <111930814+MatthewAndresWestMec@users.noreply.github.com>
2024-04-11 15:05:28 -0700

    STYLING

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,43 @@
-
-import { redirect } from 'next/navigation';
-import { login, logout } from './lib';
+import { redirect } from "next/navigation";
+import { login, logout } from "./lib";
 
 export default function SignIn() {
   return (
-    <section>
+    <div className="body-style"><div className="container">
       <form
+        className="form"
         action={async (formdata) => {
-          'use server' 
-          console.log(formdata)
+          "use server";
+          console.log(formdata);
           var success = await login(formdata);
           if (success) {
-            redirect('/home');
+            redirect("/home");
           } else {
-            console.log('login failed');
+            console.log("login failed");
           }
-        }}>
-        <input type='email' name='email' id='email' />
-        <input type='password' name='password' id='password' />
-        <input type='submit' name='submit' id='submit' />
+        }}
+      >
+        <h1 className="form__title">Welcome back</h1>
+        <div className="form__input-group">
+          <input className="form__input"  type="email" name="email" id="email" placeholder="Email" />
+          <div className="padding"></div>
+          <input className="form__input" type="password" name="password" id="password" placeholder="Password"/>
+          <div className="padding"></div>
+          <input className="form__input" type="submit" name="submit" id="submit" />
+        </div>
       </form>
 
       <form
         action={async (formdata) => {
-          'use server' 
+          "use server";
           await logout();
-          redirect('/');
-        }}>
-        <button type='submit'>Logout</button>
+          redirect("/");
+        }}
+      >
+        <button type="submit">Logout</button>
         <h1>mandre361@west-mec.org and 1234</h1>
       </form>
-    </section>
+    </div></div>
+    
   );
 }
